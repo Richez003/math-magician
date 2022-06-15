ec64443c2b1c7257cfc4b053534049e788359922
richemonday@gmail.com <richemonday@gmail.com>
2022-06-15 07:31:25 +0100

    added a navigation component

---
+++ src/component/pages/navigate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './navigate.css';
+
+class Navigate extends React.PureComponent {
+  render() {
+    return (
+      <header className="head">
+        <h1>Math Magician</h1>
+        <nav>
+          <Link to="/">HomePage|</Link>
+          <Link to="/calculator">Calculator</Link>
+          <Link to="/quote">|Quotes</Link>
+        </nav>
+      </header>
+
+    );
+  }
+}
+export default Navigate;
