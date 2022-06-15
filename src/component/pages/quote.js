5542eacd819bfcc9c10ea65a173182a34da22956
richemonday@gmail.com <richemonday@gmail.com>
2022-06-15 07:45:18 +0100

    added navigation

---
+++ src/component/pages/quote.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './quote.css';
+
+const Quotes = () => {
+  const quoteData = {
+    cite: 'Mathematics is not about numbers, equations, computations or algorithms: It is about understanding.',
+    author: '- William Paul, Thurston',
+  };
+
+  const { cite, author } = quoteData;
+
+  return (
+    <>
+      <main>
+        <div>
+          <p>{cite}</p>
+          <br />
+          <p>{author}</p>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Quotes;
