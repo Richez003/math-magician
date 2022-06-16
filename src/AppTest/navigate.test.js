8f6e3bae85e130ef5be29568d514d8706a0d1c3b
richemonday@gmail.com <richemonday@gmail.com>
2022-06-16 17:55:26 +0100

    navigate compoent testing

--- src/AppTest/navigate.test.js
+++ src/AppTest/navigate.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import Navigate from '../page/navigate'
 
-it('renders correctly', () => {
-    const tree = renderer.create(<Navigate />).toJSON();
+it('NavBar component should perform correctly on initial run', () => {
+    const tree = renderer.create(<Router><Navigate /></Router>).toJSON();
     expect(tree).toMatchSnapshot();
   });
